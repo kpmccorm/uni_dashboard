26346a6866e095f65c2c87881195f35bccefaf65
kpmccorm <kpmccorm@gmail.com>
2025-03-05 21:07:58 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,1 +1,9 @@
+import streamlit as st
+import plotly.express as px
 
+st.title('University Admissions Tracking')
+uni_data = pd.read_csv('/content/university_student_dashboard_data.csv')
+
+fig = px.bar(uni_data,x='Year',y='Enrolled',title="Enrollment by Academic Year")
+
+st.plotly_chart(fig)
