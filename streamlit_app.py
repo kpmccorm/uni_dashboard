a44e5c04470b35c4c7f248bdf67b2b49954d025a
kpmccorm <kpmccorm@gmail.com>
2025-03-07 13:00:55 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,7 @@ import pandas as pd
 import numpy as np
 import plotly.express as px
 import plotly.graph_objects as go
+import streamlit as st
 
 #app title
 st.title('University Admissions Tracking')
